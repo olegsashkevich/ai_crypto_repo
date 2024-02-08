4045cc4243ccc5546782c601b32ea3d782b47e1f
goftok <skills.lancets0n@icloud.com>
2024-02-08 16:12:52 +0100

    Added new functions from input files

--- src/service.py
+++ src/service.py
@@ -3,3 +3,29 @@ console.log('Data loaded: 204 rows');
 logging.debug('Ending process...')
 logging.debug('Starting process...')
 print('Data loaded: 898 rows')
+class AsyncGethDebug(Module):
+    """
+    https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-debug
+    """
+
+    is_async = True
+
+    _trace_transaction: Method[
+        Callable[
+            ...,
+            Awaitable[
+                Union[
+                    CallTrace, PrestateTrace, OpcodeTrace, FourByteTrace, DiffModeTrace
+                ]
+            ],
+        ]
+    ] = Method(RPC.debug_traceTransaction)
+
+    async def trace_transaction(
+        self,
+        transaction_hash: _Hash32,
+        trace_config: Optional[TraceConfig] = None,
+    ) -> Union[CallTrace, PrestateTrace, OpcodeTrace, FourByteTrace, DiffModeTrace]:
+        return await self._trace_transaction(transaction_hash, trace_config)
+
+
