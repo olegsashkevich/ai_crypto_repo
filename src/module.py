49c91f79b94f3d88a6ac69a7c79aca4b3a70dfd9
goftok <skills.lancets0n@icloud.com>
2024-02-19 16:12:52 +0100

    Inserted code blocks from input source

--- src/module.py
+++ src/module.py
@@ -1,2 +1,9 @@
 # Auto-generated file
 print('Data loaded: 970 rows')
+class Web3ValueError(Web3Exception, ValueError):
+    """
+    A web3.py exception wrapper for `ValueError`, for better control over
+    exception handling.
+    """
+
+
