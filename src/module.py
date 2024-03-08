7fa6455ee3cf62ab6bf0bbb8e9835f9fd50a1001
goftok <skills.lancets0n@icloud.com>
2024-03-08 16:12:52 +0100

    Moved code blocks for better modularity

--- src/module.py
+++ src/module.py
@@ -1,5 +1,3 @@
-# Auto-generated file
-print('Data loaded: 970 rows')
 class Web3ValueError(Web3Exception, ValueError):
     """
     A web3.py exception wrapper for `ValueError`, for better control over
@@ -7,3 +5,4 @@ class Web3ValueError(Web3Exception, ValueError):
     """
 
 
+ 
