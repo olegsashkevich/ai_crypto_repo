aca76e9491bda32758e9c9e66c4dce8df992e9a5
goftok <skills.lancets0n@icloud.com>
2024-04-07 16:12:52 +0200

    Reorganized functions for clarity

--- src/service.py
+++ src/service.py
@@ -1,29 +1,3 @@
-class AsyncGethDebug(Module):
-    """
-    https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-debug
-    """
-
-    is_async = True
-
-    _trace_transaction: Method[
-        Callable[
-            ...,
-            Awaitable[
-                Union[
-                    CallTrace, PrestateTrace, OpcodeTrace, FourByteTrace, DiffModeTrace
-                ]
-            ],
-        ]
-    ] = Method(RPC.debug_traceTransaction)
-
-    async def trace_transaction(
-        self,
-        transaction_hash: _Hash32,
-        trace_config: Optional[TraceConfig] = None,
-    ) -> Union[CallTrace, PrestateTrace, OpcodeTrace, FourByteTrace, DiffModeTrace]:
-        return await self._trace_transaction(transaction_hash, trace_config)
-
-
 class Module:
     is_async = False
 
@@ -138,5 +112,31 @@ class Module:
 
     async def stop_ws(self) -> bool:
         return await self._stop_ws()
+
+
+class AsyncGethDebug(Module):
+    """
+    https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-debug
+    """
+
+    is_async = True
+
+    _trace_transaction: Method[
+        Callable[
+            ...,
+            Awaitable[
+                Union[
+                    CallTrace, PrestateTrace, OpcodeTrace, FourByteTrace, DiffModeTrace
+                ]
+            ],
+        ]
+    ] = Method(RPC.debug_traceTransaction)
+
+    async def trace_transaction(
+        self,
+        transaction_hash: _Hash32,
+        trace_config: Optional[TraceConfig] = None,
+    ) -> Union[CallTrace, PrestateTrace, OpcodeTrace, FourByteTrace, DiffModeTrace]:
+        return await self._trace_transaction(transaction_hash, trace_config)
 
 
