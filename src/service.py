57f2f0135b6783bd8b3a49123dc840cb4cd2bf3d
goftok <skills.lancets0n@icloud.com>
2024-04-08 16:12:52 +0200

    Restructured code for better readability

--- src/service.py
+++ src/service.py
@@ -24,7 +24,35 @@ class AsyncGethDebug(Module):
         return await self._trace_transaction(transaction_hash, trace_config)
 
 
-class AsyncGethAdmin(Module):
+class Module:
+    is_async = False
+
+    def __init__(self, w3: Union["AsyncWeb3", "Web3"]) -> None:
+        if self.is_async:
+            self.retrieve_caller_fn = retrieve_async_method_call_fn(w3, self)
+        else:
+            self.retrieve_caller_fn = retrieve_blocking_method_call_fn(w3, self)
+        self.retrieve_request_information = retrieve_request_information_for_batching(
+            w3, self
+        )
+        self.w3 = w3
+
+    @property
+    def codec(self) -> ABICodec:
+        # use codec set on the Web3 instance
+        return self.w3.codec
+
+    def attach_methods(
+        self,
+        methods: Dict[str, Method[Callable[..., Any]]],
+    ) -> None:
+        for method_name, method_class in methods.items():
+            klass = (
+                method_class.__get__(module=self)()
+                if method_class.is_property
+                else method_class.__get__(module=self)
+            )
+            setattr(self, method_name, klass)class AsyncGethAdmin(Module):
     """
     https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-admin
     """
@@ -112,32 +140,3 @@ class AsyncGethAdmin(Module):
         return await self._stop_ws()
 
 
-class Module:
-    is_async = False
-
-    def __init__(self, w3: Union["AsyncWeb3", "Web3"]) -> None:
-        if self.is_async:
-            self.retrieve_caller_fn = retrieve_async_method_call_fn(w3, self)
-        else:
-            self.retrieve_caller_fn = retrieve_blocking_method_call_fn(w3, self)
-        self.retrieve_request_information = retrieve_request_information_for_batching(
-            w3, self
-        )
-        self.w3 = w3
-
-    @property
-    def codec(self) -> ABICodec:
-        # use codec set on the Web3 instance
-        return self.w3.codec
-
-    def attach_methods(
-        self,
-        methods: Dict[str, Method[Callable[..., Any]]],
-    ) -> None:
-        for method_name, method_class in methods.items():
-            klass = (
-                method_class.__get__(module=self)()
-                if method_class.is_property
-                else method_class.__get__(module=self)
-            )
-            setattr(self, method_name, klass)
