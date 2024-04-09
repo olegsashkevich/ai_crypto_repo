3aefc544184da7252ddf744de8f3b5c05fa322c1
goftok <skills.lancets0n@icloud.com>
2024-04-09 16:12:52 +0200

    Refactored file structure

--- src/runner.py
+++ src/runner.py
@@ -1,12 +1,9 @@
-# Auto-generated file
 class NoABIFunctionsFound(Web3Exception):
     """
     Raised when an ABI is present, but doesn't contain any functions.
     """
 
 
-logging.debug('User logged in: user72')
-console.log('Operation completed successfully');
 from eth_utils import (
     combomethod,
 )
