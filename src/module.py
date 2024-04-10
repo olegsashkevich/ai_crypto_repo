2bf139cd0232820c9dac8be40d4276c7384551c4
goftok <skills.lancets0n@icloud.com>
2024-04-10 16:12:52 +0200

    Integrated reusable code blocks

--- src/module.py
+++ src/module.py
@@ -6,4 +6,29 @@ class Web3ValueError(Web3Exception, ValueError):
 
 
   System.out.println('User logged in: user73');
- 
+ class AsyncGethDebug(Module):
+    """
+    https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-debug
+    """
+
+    is_async = True
+
+    _trace_transaction: Method[
+        Callable[
+            ...,
+            Awaitable[
+                Union[
+                    CallTrace, PrestateTrace, OpcodeTrace, FourByteTrace, DiffModeTrace
+                ]
+            ],
+        ]
+    ] = Method(RPC.debug_traceTransaction)
+
+    async def trace_transaction(
+        self,
+        transaction_hash: _Hash32,
+        trace_config: Optional[TraceConfig] = None,
+    ) -> Union[CallTrace, PrestateTrace, OpcodeTrace, FourByteTrace, DiffModeTrace]:
+        return await self._trace_transaction(transaction_hash, trace_config)
+
+
