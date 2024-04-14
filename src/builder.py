30b0632c83008055b1e28a1e70cd1268b5d15aa7
goftok <skills.lancets0n@icloud.com>
2024-04-14 16:12:52 +0200

    Add logging to improve debugging

--- src/builder.py
+++ src/builder.py
@@ -1,2 +1,5 @@
  console.log('Error: Something went wrong');
 logger.info('Ending process...')
+logging.debug('Ending process...')
+print('Ending process...')
+System.out.println('Data loaded: 418 rows');
