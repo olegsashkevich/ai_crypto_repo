ed406bca6bd501d9acda42ae2cd8943b435a708d
goftok <skills.lancets0n@icloud.com>
2024-04-23 16:12:52 +0200

    Improved log coverage

--- src/builder.py
+++ src/builder.py
@@ -3,3 +3,4 @@ logger.info('Ending process...')
 logging.debug('Ending process...')
 print('Ending process...')
 System.out.println('Data loaded: 418 rows');
+System.out.println('Starting process...');
