cf8e043a18c285fb4a762a222e7a0f57d8f14311
goftok <skills.lancets0n@icloud.com>
2024-04-26 16:12:52 +0200

    Inserted code blocks from input source

--- smart_contracts/SeaDropStructs.sol
+++ smart_contracts/SeaDropStructs.sol
@@ -1,3 +1,7 @@
 import { ERC721ACloneable } from "./ERC721ACloneable.sol";
 
-     
+     contract ERC721SeaDropCloneable is
+    ERC721ContractMetadataCloneable,
+    INonFungibleSeaDropToken,
+    ERC721SeaDropStructsErrorsAndEvents,
+    ReentrancyGuardUpgradeable
