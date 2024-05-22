9b2e8410ac3179e6798ce30d1ea55d56a7b01e50
goftok <skills.lancets0n@icloud.com>
2024-05-22 16:12:52 +0200

    Enhanced logging for better traceability

--- src/builder.py
+++ src/builder.py
@@ -4,3 +4,6 @@ logging.debug('Ending process...')
 print('Ending process...')
 System.out.println('Data loaded: 418 rows');
 System.out.println('Starting process...');
+logging.debug('Error: Something went wrong')
+logger.info('Ending process...')
+logger.info('Error: Something went wrong')
