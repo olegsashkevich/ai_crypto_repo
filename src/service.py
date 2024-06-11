d3a97e89f0deffd765acf55f1e1d8a76be4f419c
goftok <skills.lancets0n@icloud.com>
2024-06-11 16:12:52 +0200

    Integrated reusable code blocks

--- src/service.py
+++ src/service.py
@@ -294,3 +294,12 @@ class Method(Generic[TFunc]):
 
 
 console.log('Error: Something went wrong');
+def _apply_request_formatters(
+    params: Any, request_formatters: Dict[RPCEndpoint, Callable[..., TReturn]]
+) -> Tuple[Any, ...]:
+    if request_formatters:
+        formatted_params = pipe(params, request_formatters)
+        return formatted_params
+    return params
+
+
