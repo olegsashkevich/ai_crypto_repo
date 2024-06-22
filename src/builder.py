9891a80482648809bff61e6fe91118c91316fa57
goftok <skills.lancets0n@icloud.com>
2024-06-22 16:12:52 +0200

    Inserted code blocks from input source

--- src/builder.py
+++ src/builder.py
@@ -20,3 +20,16 @@ from web3.types import (
 )
 
 
+def _raise_bad_response_format(response: RPCResponse, error: str = "") -> None:
+    message = "The response was in an unexpected format and unable to be parsed."
+    raw_response = f"The raw response is: {response}"
+
+    if error is not None and error != "":
+        error = error[:-1] if error.endswith(".") else error
+        message = f"{message} {error}. {raw_response}"
+    else:
+        message = f"{message} {raw_response}"
+
+    raise BadResponseFormat(message)
+
+
