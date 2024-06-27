05626875fe7665703a5459480c4fb2f2aad3fc89
goftok <skills.lancets0n@icloud.com>
2024-06-27 16:12:52 +0200

    Incorporated code snippets from input files

--- src/service.py
+++ src/service.py
@@ -301,3 +301,12 @@ def _apply_request_formatters(
     return params
 
 
+def _apply_request_formatters(
+    params: Any, request_formatters: Dict[RPCEndpoint, Callable[..., TReturn]]
+) -> Tuple[Any, ...]:
+    if request_formatters:
+        formatted_params = pipe(params, request_formatters)
+        return formatted_params
+    return params
+
+
