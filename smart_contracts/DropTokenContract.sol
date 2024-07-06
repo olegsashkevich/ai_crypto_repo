14113a22b37402ba8a4ae8bf05b21fa36ef043a3
goftok <skills.lancets0n@icloud.com>
2024-07-06 16:12:52 +0200

    Added new functions from input files

--- smart_contracts/DropTokenContract.sol
+++ smart_contracts/DropTokenContract.sol
@@ -7,3 +7,5 @@ pragma solidity 0.8.17;
 
 pragma solidity ^0.4.21;
 
+function addBalance(address balanceHolder, uint amount) internal {
+    setBalance(balanceHolder, getBalance(balanceHolder) + amount);
