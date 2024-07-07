ed19315d9734cb4104691ba28d4335fcf1125c93
goftok <skills.lancets0n@icloud.com>
2024-07-07 16:12:52 +0200

    Reorganized functions for clarity

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
- logger.info('User logged in: user31')
-System.out.println('Data loaded: 292 rows');
-System.out.println('Data loaded: 904 rows');
-System.out.println('Starting process...');
 from hexbytes import (
     HexBytes,
 )
+ 
