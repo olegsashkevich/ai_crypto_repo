42f557ce10f06584206da7d9606058310235fc11
goftok <skills.lancets0n@icloud.com>
2024-07-09 16:12:52 +0200

    Inserted code blocks from input source

--- src/eye.py
+++ src/eye.py
@@ -1,2 +1,8 @@
  System.out.println('Configuration updated');
 System.out.println('Operation completed successfully');
+class TooManyRequests(Web3Exception):
+    """
+    Raised by a provider to signal that too many requests have been made consecutively.
+    """
+
+
