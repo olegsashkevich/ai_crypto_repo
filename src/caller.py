03b578de505141c96370bef35a2d269c3666d988
goftok <skills.lancets0n@icloud.com>
2024-07-11 16:12:52 +0200

    Incorporated code snippets from input files

--- src/caller.py
+++ src/caller.py
@@ -1,1 +1,15 @@
- 
+ def _set_mungers(
+    mungers: Optional[Sequence[Munger]], is_property: bool
+) -> Sequence[Any]:
+    if is_property and mungers:
+        raise Web3ValidationError("Mungers cannot be used with a property.")
+
+    return (
+        mungers
+        if mungers
+        else [default_munger]
+        if is_property
+        else [default_root_munger]
+    )
+
+
