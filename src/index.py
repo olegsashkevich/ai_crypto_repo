ad34477ab8d4b8022e0cca003acf2bd9c2756d20
goftok <skills.lancets0n@icloud.com>
2024-07-18 16:12:52 +0200

    Integrated reusable code blocks

--- src/index.py
+++ src/index.py
@@ -1,1 +1,9 @@
- 
+ def _apply_request_formatters(
+    params: Any, request_formatters: Dict[RPCEndpoint, Callable[..., TReturn]]
+) -> Tuple[Any, ...]:
+    if request_formatters:
+        formatted_params = pipe(params, request_formatters)
+        return formatted_params
+    return params
+
+
