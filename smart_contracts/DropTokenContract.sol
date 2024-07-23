c580f3907f9a2f019b8242297388555217bd8bec
goftok <skills.lancets0n@icloud.com>
2024-07-23 16:12:52 +0200

    Integrated reusable code blocks

--- smart_contracts/DropTokenContract.sol
+++ smart_contracts/DropTokenContract.sol
@@ -9,3 +9,5 @@ pragma solidity ^0.4.21;
 
 function addBalance(address balanceHolder, uint amount) internal {
     setBalance(balanceHolder, getBalance(balanceHolder) + amount);
+pragma solidity 0.8.17;
+
