0d8902ce81bbdc9490e3357eafed2276815f9eef
goftok <skills.lancets0n@icloud.com>
2024-08-06 16:12:52 +0200

    Added new functions from input files

--- src/node.py
+++ src/node.py
@@ -8,4 +8,9 @@ class TimeExhausted(Web3Exception):
   from web3._utils.batching import (
     RPC_METHODS_UNSUPPORTED_DURING_BATCH,
 )
-        
+        class ABIConstructorNotFound(Web3Exception):
+    """
+    Raised when a constructor function doesn't exist in contract.
+    """
+
+
