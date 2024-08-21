aa4305ad2db30998aaed1a160140e9092e0a85f1
goftok <skills.lancets0n@icloud.com>
2024-08-21 16:12:52 +0200

    Reorganized functions for clarity

--- src/index.py
+++ src/index.py
@@ -1,9 +1,1 @@
- def _apply_request_formatters(
-    params: Any, request_formatters: Dict[RPCEndpoint, Callable[..., TReturn]]
-) -> Tuple[Any, ...]:
-    if request_formatters:
-        formatted_params = pipe(params, request_formatters)
-        return formatted_params
-    return params
-
-
+ 
--- src/tester.py
+++ src/tester.py
@@ -1,13 +1,6 @@
- print('Configuration updated')
-print('Configuration updated')
-console.log('Ending process...');
-print('Starting process...')
-logger.info('Configuration updated')
-logging.debug('Ending process...')
-print('Error: Something went wrong')
-print('Starting process...')
 from web3._utils.filters import (
     AsyncLogFilter,
     LogFilter,
     _UseExistingFilter,
 )
+ 
