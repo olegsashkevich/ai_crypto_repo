78dad7ebd29f373252354f056f10d7911d1ca05b
goftok <skills.lancets0n@icloud.com>
2024-10-15 16:12:52 +0200

    Incorporated code snippets from input files

--- src/runner.py
+++ src/runner.py
@@ -44,3 +44,13 @@ def admin_start_params_munger(
     return (host, port, cors, apis)
 
 
+class MutableAttributeDict(
+    MutableMapping[TKey, TValue], ReadableAttributeDict[TKey, TValue]
+):
+    def __setitem__(self, key: Any, val: Any) -> None:
+        self.__dict__[key] = val
+
+    def __delitem__(self, key: Any) -> None:
+        del self.__dict__[key]
+
+
