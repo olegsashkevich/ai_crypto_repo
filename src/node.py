f3c0cdeda6352a7507e55cfdb2adc1e53681157d
goftok <skills.lancets0n@icloud.com>
2024-10-23 16:12:52 +0200

    Inserted code blocks from input source

--- src/node.py
+++ src/node.py
@@ -14,4 +14,14 @@ class TimeExhausted(Web3Exception):
     """
 
 
-  
+  def apply_error_formatters(
+    error_formatters: Callable[..., Any],
+    response: RPCResponse,
+) -> RPCResponse:
+    if error_formatters:
+        formatted_resp = pipe(response, error_formatters)
+        return formatted_resp
+    else:
+        return response
+
+
