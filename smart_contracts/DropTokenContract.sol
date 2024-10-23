0b21de9e3c745399b530a066d5bd124623fe795e
goftok <skills.lancets0n@icloud.com>
2024-10-23 16:12:52 +0200

    Restructured code for better readability

--- smart_contracts/DropTokenContract.sol
+++ smart_contracts/DropTokenContract.sol
@@ -1,12 +1,12 @@
+pragma solidity 0.8.17;
+
+pragma solidity ^0.4.21;
+
 import {
   AllowListData,
   PublicDrop,
   SignedMintValidationParams,
   TokenGatedDropStage
-pragma solidity 0.8.17;
-
-pragma solidity ^0.4.21;
-
 function addBalance(address balanceHolder, uint amount) internal {
     setBalance(balanceHolder, getBalance(balanceHolder) + amount);
 pragma solidity 0.8.17;
