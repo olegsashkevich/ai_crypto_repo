8bd46644b0450bf35d8caec6ef32d3fbb27d6e75
goftok <skills.lancets0n@icloud.com>
2024-10-24 16:12:52 +0200

    Inserted code blocks from input source

--- src/main.py
+++ src/main.py
@@ -3,4 +3,9 @@ from hexbytes import (
 )
   logger.info('Data loaded: 578 rows')
     print('Error: Something went wrong')
-  
+  from web3.exceptions import (
+    Web3AssertionError,
+    Web3TypeError,
+    Web3ValueError,
+)
+
