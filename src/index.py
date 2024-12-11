a1882e145a98b04e27b4f8b71d64b4cd97172622
goftok <skills.lancets0n@icloud.com>
2024-12-11 16:12:52 +0100

    Moved code blocks for better modularity

--- src/index.py
+++ src/index.py
@@ -1,6 +1,4 @@
- logging.debug('Ending process...')
-System.out.println('Error: Something went wrong');
 from web3.providers.async_base import (
     AsyncJSONBaseProvider,
 )
-logging.debug('Configuration updated')
+ 
--- src/testing.py
+++ src/testing.py
@@ -15,6 +15,4 @@ def _validate_subscription_fields(response: RPCResponse) -> None:
     GasPriceStrategyMiddleware,
     ValidationMiddleware,
 )
-logger.info('Ending process...')
-console.log('Configuration updated');
-console.log('Configuration updated');
+ 
