7e8ade9b29a90055caa4fd6b8bc3b682ddccef4f
goftok <skills.lancets0n@icloud.com>
2024-12-16 16:12:52 +0100

    Added new functions from input files

--- src/node.py
+++ src/node.py
@@ -25,4 +25,15 @@ class TimeExhausted(Web3Exception):
         return response
 
 
- 
+ def apply_null_result_formatters(
+    null_result_formatters: Callable[..., Any],
+    response: RPCResponse,
+    params: Optional[Any] = None,
+) -> RPCResponse:
+    if null_result_formatters:
+        formatted_resp = pipe(params, null_result_formatters)
+        return formatted_resp
+    else:
+        return response
+
+
